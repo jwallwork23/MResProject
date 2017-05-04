a61ab5ad30fb1b5eef506923532a01b60a5d7826
Joseph Wallwork <j.wallwork16@imperial.ac.uk>
2017-05-04 12:04:32 +0100

    Incorporated bathymetry into tank_adaptive

--- tank_adaptive.py
+++ tank_adaptive.py
@@ -8,30 +8,33 @@ from forms import *
 from interp import interp
 
 # Specify problem parameters:
-mode = raw_input('Use linear or nonlinear equations? (l/n): ') or 'l'
+mode = raw_input('Linear or nonlinear equations? (l/n): ') or 'l'
 if ((mode != 'l') & (mode != 'n')):
     raise ValueError('Please try again, choosing l or n.')
-dt = float(raw_input('Timestep (default 0.1)?: ') or 0.1)
+bathy = raw_input('Non-trivial bathymetry? (y/n): ') or 'n'
+if ((bathy != 'y') & (bathy != 'n')):
+    raise ValueError('Please try again, choosing y or n.')
+dt = float(raw_input('Timestep (default 0.1)?: ') or 0.1)               # TODO: consider adaptive timestepping?
 Dt = Constant(dt)
-n = int(raw_input('Number of mesh cells per m (default 16)?: ') or 16)
-n2 = n**2
+n = int(raw_input('Mesh cells per m (default 16)?: ') or 16)
 T = float(raw_input('Simulation duration in s (default 5)?: ') or 5.0)
 remesh = raw_input('Use adaptive meshing (y/n)?: ') or 'y'
-if ((remesh != 'y') & (remesh != 'n')):
-    raise ValueError('Please try again, typing y or n.')
-
-# Set numerical parameters for the scheme:
-ndump = 1       # Timesteps per data dump
-rm = 6          # Timesteps per remesh
+if (remesh == 'y'):
+    rm = int(raw_input('Timesteps per remesh (default 6)?: ') or 6)     # TODO: consider adaptive remeshing?
+else:
+    rm = int(T/dt)
+    if (remesh != 'n'):
+        raise ValueError('Please try again, typing y or n.')
+ndump = 1
 
 # Establish tank domain
-mesh, Vq, q_, u_, eta_, lam_, lu_, le_, b, BCs = tank_domain(n)
+mesh, Vq, q_, u_, eta_, lam_, lu_, le_, b, BCs = tank_domain(n, bath=bathy)
 
 # Initialisation:
 t = 0.0; dumpn = 0; mn = 0
 q = Function(Vq)
 q.assign(q_)
-q_file = File('plots/prob1_test_outputs/prob1_adapt.pvd'.format(y=mn))
+q_file = File('plots/prob1_test_outputs/prob1_adapt.pvd')
 params = {'mat_type': 'matfree',
           'snes_type': 'ksponly',
           'pc_type': 'python',
@@ -43,8 +46,9 @@ params = {'mat_type': 'matfree',
 while (t < T-0.5*dt):
 
     mn += 1
+    step_file = File('plots/prob1_test_outputs/prob1_adapt_step_{y}.pvd'.format(y=mn))  # TODO: get rid of this
 
-    if (t != 0.0):
+    if (t != 0.0):                                                      # TODO: Could adapt straight away?
 
         # Set up metric:
         Vm = TensorFunctionSpace(mesh, 'CG', 1)
@@ -55,7 +59,7 @@ while (t < T-0.5*dt):
         if (remesh == 'y'):
             M = compute_steady_metric(mesh, V, H, eta)
         else:
-            M.interpolate(Expression([[n2, 0], [0, n2]]))
+            M.interpolate(Expression([[n*n, 0], [0, n*n]]))
 
         # Adapt mesh and update FE setup:
         mesh_ = mesh
@@ -71,6 +75,7 @@ while (t < T-0.5*dt):
 
     if (t == 0.0):
         q_file.write(u, eta, time=t)
+        step_file.write(u, eta, time=t)                                 # TODO: get rid of this
     cnt = 0
 
     # Enter the inner timeloop:
@@ -84,3 +89,4 @@ while (t < T-0.5*dt):
         if (dumpn == ndump):
             dumpn -= ndump
             q_file.write(u, eta, time=t)
+            step_file.write(u, eta, time=t)                             # TODO: get rid of this

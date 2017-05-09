e2f4e35fe50b88aa53ec92d722b6f9b4bdc9fab1
Joseph Wallwork <j.wallwork16@imperial.ac.uk>
2017-05-09 08:52:09 +0100

    Attempt to integrate 2D tsunami tests into one file

--- 2D_tsunami_test_adaptive.py
+++ 2D_tsunami_test_adaptive.py
@@ -49,9 +49,10 @@ t = 0.0
 dumpn = 0
 mn = 0
 cnt = 0
+i = 0
 q = Function(Vq)
 q.assign(q_)
-q_file = File('plots/adjoint_test_outputs/linear_forward_adaptive.pvd')
+q_file = File('plots/adjoint_test_outputs/linear_forward.pvd')
 params = {'mat_type': 'matfree',
           'snes_type': 'ksponly',
           'pc_type': 'python',
@@ -80,6 +81,23 @@ mu, eta = q.split()
 mu.rename('Fluid momentum')
 eta.rename('Free surface displacement')
 q_file.write(mu, eta, time=t)
+
+if remesh == 'n':
+
+    # Establish a BC object to get 'coastline'
+    bc = DirichletBC(Vq.sub(1), 0, 1)
+    b_nodes = bc.nodes
+
+    # Initialise a CG1 version of mu and some arrays for storage:
+    V1 = VectorFunctionSpace(mesh, 'CG', 1)
+    mu_cg1 = Function(V1)
+    mu_cg1.dat.data[:] = mu.at(mesh.coordinates.dat.data)
+    eta_vals = np.zeros((int(T/(ndump*dt))+1, (nx+1)**2))
+    mu_vals = np.zeros((int(T/(ndump*dt))+1, (nx+1)**2, 2))
+    m = np.zeros((int(T/(ndump*dt))+1))
+    eta_vals[i,:] = eta.dat.data
+    mu_vals[i,:,:] = mu_cg1.dat.data
+    m[i] = np.log2(max(max(eta_vals[i, b_nodes]), 0.5))
 
 # Enter the forward timeloop:
 while t < T - 0.5*dt:
@@ -132,23 +150,34 @@ while t < T - 0.5*dt:
             dumpn -= ndump
             q_file.write(mu, eta, time=t)
 
+            if remesh == 'n':
+                i += 1
+                mu_cg1.interpolate(mu)
+                mu_vals[i, :, :] = mu_cg1.dat.data
+                eta_vals[i, :] = eta.dat.data
+
+                # Implement damage measures:
+                m[i] = np.log2(max(max(eta_vals[i, b_nodes]), 0.5))
+
 print 'Forward problem solved.... now for the adjoint problem.'
 
-# Reset uniform mesh:
-#mesh = SquareMesh(nx, nx, lx, lx)
+if remesh == 'y':
 
-# Re-define function spaces:
-Vu = VectorFunctionSpace(mesh, 'CG', 2)     # \ Taylor-Hood elements
-Ve = FunctionSpace(mesh, 'CG', 1)           # /
-Vq = MixedFunctionSpace((Vu, Ve))           # Mixed FE problem
+    # Reset uniform mesh:
+    mesh = SquareMesh(nx, nx, lx, lx)
+
+    # Re-define function spaces:
+    Vu = VectorFunctionSpace(mesh, 'CG', 2)     # \ Taylor-Hood elements
+    Ve = FunctionSpace(mesh, 'CG', 1)           # /
+    Vq = MixedFunctionSpace((Vu, Ve))           # Mixed FE problem
+
+    # Re-establish bathymetry function:
+    b = Function(Ve, name = 'Bathymetry')
+    b.interpolate(Expression('x[0] <= 50000. ? 200. : 4000.'))  # Shelf break bathymetry
 
 # Construct a function to store our two variables at time n:
 lam_ = Function(Vq)         # Adjoint solution tuple
 lu_, le_ = lam_.split()
-
-# Re-establish bathymetry function:
-b = Function(Ve, name = 'Bathymetry')
-b.interpolate(Expression('x[0] <= 50000. ? 200. : 4000.'))  # Shelf break bathymetry
 
 # Interpolate initial and boundary conditions, noting higher magnitude wave used due to geometric spreading:
 lu_.interpolate(Expression([0, 0]))
@@ -159,7 +188,7 @@ cnt = 0
 mn = 0
 lam = Function(Vq)
 lam.assign(lam_)
-lam_file = File('plots/adjoint_test_outputs/linear_adjoint_adaptive.pvd')
+lam_file = File('plots/adjoint_test_outputs/linear_adjoint.pvd')
 
 # Set up functions of weak problem:
 w, xi = TestFunctions(Vq)
@@ -177,14 +206,36 @@ lam_solv = NonlinearVariationalSolver(lam_prob, solver_parameters=params)
 lm_, le_ = lam_.split()
 lm, le = lam.split()
 
+# Relabel and initialise file:
 lm.rename('Adjoint fluid momentum')
 le.rename('Adjoint free surface displacement')
+lam_file.write(lm, le, time=0)
 
-# Initialise dump counter and files:
+if remesh == 'n':
+
+    # Initialise a CG1 version of lm and some arrays for storage:
+    lm_cg1 = Function(V1)
+    lm_cg1.interpolate(lm)
+    le_vals = np.zeros((int(T/(ndump*dt))+1, (nx+1)**2))
+    lm_vals = np.zeros((int(T/(ndump*dt))+1, (nx+1)**2, 2))
+    ql_vals = np.zeros((int(T/(ndump*dt))+1, (nx+1)**2))
+    q_dot_lam = Function(Vq.sub(1))
+    q_dot_lam.rename('Forward-adjoint inner product')
+    le_vals[i,:] = le.dat.data
+    lm_vals[i,:] = lm_cg1.dat.data
+
+    # Initialise file:
+    dot_file = File('plots/adjoint_test_outputs/inner_product.pvd')
+    dot_file.write(q_dot_lam, time=0)
+
+    # Evaluate forward-adjoint inner products (noting mu and lm are in P2, while eta and le are in P1, so we need to
+    # evaluate at nodes):
+    ql_vals[i,:] = mu_vals[i,:,0] * lm_vals[i,:,0] + mu_vals[i,:,1] * lm_vals[i,:,1] + eta_vals[i,:] * le_vals[i,:]
+    q_dot_lam.dat.data[:] = ql_vals[i,:]
+
+# Initialise dump counter:
 if dumpn == 0:
     dumpn = ndump
-
-lam_file.write(lm, le, time=0)
 
 # Enter the backward timeloop:
 while t > 0:
@@ -235,4 +286,39 @@ while t > 0:
         dumpn -= 1
         if dumpn == ndump:
             dumpn += ndump
-            lam_file.write(lm, le, time=T-t)    # Note the time inversion
+            # Write to file, noting time inversion:
+            lam_file.write(lm, le, time=T-t)
+
+            if remesh == 'n':
+                i -= 1
+                lm_cg1.interpolate(lm)
+                lm_vals[i, :] = lm_cg1.dat.data
+                le_vals[i, :] = le.dat.data
+                ql_vals[i, :] = mu_vals[i, :, 0] * lm_vals[i, :, 0] + mu_vals[i, :, 1] * lm_vals[i, :, 1] + \
+                                eta_vals[i, :] * le_vals[i, :]
+                q_dot_lam.dat.data[:] = ql_vals[i, :]
+                dot_file.write(q_dot_lam, time=T-t)
+
+# Plot damage measures:
+if remesh == 'n':
+    plt.rc('text', usetex=True)
+    plt.rc('font', family='serif')
+    plt.plot(range(0, int(T)+1, int(ndump*dt)), m, color='black')
+    plt.axis([0, T, -1.5, 3.5])
+    plt.axhline(-1, linestyle='--', color='blue')
+    plt.axhline(0, linestyle='--', color='green')
+    plt.axhline(1, linestyle='--', color='yellow')
+    plt.axhline(2, linestyle='--', color='orange')
+    plt.axhline(3, linestyle='--', color='red')
+    plt.annotate('Severe damage', xy=(0.7*T, 3), xytext=(0.72*T, 3.2), arrowprops=dict(facecolor='red', shrink=0.05))
+    plt.annotate('Some inland damage', xy=(0.7*T, 2), xytext=(0.72*T, 2.2),
+                 arrowprops=dict(facecolor='orange', shrink=0.05))
+    plt.annotate('Shore damage', xy=(0.7*T, 1), xytext=(0.72*T, 1.2),
+                 arrowprops=dict(facecolor='yellow', shrink=0.05))
+    plt.annotate('Very little damage', xy=(0.7*T, 0), xytext=(0.72*T, 0.2),
+                 arrowprops=dict(facecolor='green', shrink=0.05))
+    plt.annotate('No damage', xy=(0.7*T, -1), xytext=(0.72*T, -0.8), arrowprops=dict(facecolor='blue', shrink=0.05))
+    plt.xlabel(r'Time (s)')
+    plt.ylabel(r'm (dimensionless)')
+    plt.title(r'Damage measures')
+    plt.savefig('plots/tsunami_outputs/screenshots/2Ddamage_measures.png')

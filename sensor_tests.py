1acd363ef0e0893cb21051da609390788ff4f2ae
Joseph Wallwork <j.wallwork16@imperial.ac.uk>
2017-06-21 15:11:38 +0100

    Included multiple remeshes in sensor tests

--- sensor_tests.py
+++ sensor_tests.py
@@ -5,10 +5,11 @@ from time import clock
 from utils import adapt, construct_hessian, compute_steady_metric, interp, Meshd
 
 # Specify problem parameters:
-hmin = float(raw_input('Minimum element size (default 0.005)?: ') or 0.005)
+hmin = float(raw_input('Minimum element size (default 0.001)?: ') or 0.001)
+N = int(raw_input('Number of adaptions (default 4)?: ') or 4)
 
 # Define uniform mesh, with a metric function space:
-mesh1 = SquareMesh(100, 100, 2, 2)
+mesh1 = SquareMesh(500, 500, 2, 2)
 meshd1 = Meshd(mesh1)
 x, y = SpatialCoordinate(mesh1)
 x = x - 1
@@ -28,34 +29,45 @@ f2.interpolate(Expression('abs((x[0]-1) * (x[1]-1)) >= pi/25. ? \
 f3.interpolate(0.1 * sin(50 * x) + atan(0.1 / (sin(5 * y) - 2 * x)))
 f4.interpolate(atan(0.1 / (sin(5 * y) - 2 * x)) + atan(0.5 / (sin(3 * y) - 7 * x)))
 f = {1: f1, 2: f2, 3: f3, 4: f4}
+print ''
 
 for i in f:
 
-    tic1 = clock()
+    mesh = mesh1
+    V = TensorFunctionSpace(mesh1, 'CG', 1)
 
-    # Compute Hessian and metric:
-    H = construct_hessian(mesh1, V, f[i])
-    M = compute_steady_metric(mesh1, V, H, f[i], h_min = hmin, a = 1000.)
+    print '*********** Sensor %d ***********' % i
 
-    # Adapt mesh and set up new function spaces:
-    mesh2 = adapt(mesh1, M)
-    meshd2 = Meshd(mesh2)
-    G = FunctionSpace(mesh2, 'CG', 1)
+    for j in range(N) :
 
-    # Interpolate functions onto new mesh:
-    g = Function(G)
-    interp(f[i], meshd1, g, meshd2)
-    toc1 = clock()
+        tic1 = clock()
 
-    # Print to screen:
-    print ''
-    print '*********** Sensor {y} ***********'.format(y=i)
-    print 'Number of nodes after adaption : ', len(mesh2.coordinates.dat.data)
-    print 'Elapsed time adapting to sensor %d: %1.2es' % (i, toc1 - tic1)
-    print ''
+        # Compute Hessian and metric:
+        H = construct_hessian(mesh, V, f[i])
+        M = compute_steady_metric(mesh, V, H, f[i], h_min = hmin, a = 1000.)
 
-    # Plot results:
-    g.rename('Sensor {y}'.format(y=i))
-    M.rename('Metric field {y}'.format(y=i))
-    File('plots/adapt_plots/sensor_test{y}.pvd'.format(y=i)).write(g)
-    File('plots/adapt_plots/sensor_test_metric{y}.pvd'.format(y=i)).write(M)
+        # Adapt mesh and set up new function spaces:
+        mesh_ = mesh
+        mesh = adapt(mesh, M)
+        meshd_ = Meshd(mesh_)
+        meshd = Meshd(mesh)
+        G = FunctionSpace(mesh, 'CG', 1)
+
+        # Interpolate functions onto new mesh:
+        g = Function(G)
+        interp(f[i], meshd_, g, meshd)
+        V = TensorFunctionSpace(mesh, 'CG', 1)
+        f[i] = g
+        toc1 = clock()
+
+        # Print to screen:
+        print ''
+        print 'Number of nodes after adaption %d : %d ' % (j, len(mesh.coordinates.dat.data))
+        print 'Elapsed time for this step: %1.2es' % (toc1 - tic1)
+        print ''
+
+        # Plot results:
+        g.rename('Sensor {y}'.format(y = i))
+        M.rename('Metric field {y}'.format(y = i))
+        File('plots/adapt_plots/sensor_test{y}.pvd'.format(y = i)).write(g)
+        File('plots/adapt_plots/sensor_test_metric{y}.pvd'.format(y = i)).write(M)

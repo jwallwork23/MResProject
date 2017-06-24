45dcda91a38a304ac83684ab29d290bf14fc3976
Joseph Wallwork <j.wallwork16@imperial.ac.uk>
2017-06-24 15:15:08 +0100

    Plotting function created

--- utils/storage.py
+++ utils/storage.py
@@ -1,9 +1,5 @@
-def wrapper(func, *args, **kwargs) :
-    """A wrapper function to enable timing of functions with arguments"""
-    def wrapped() :
-        return func(*args, **kwargs)
-    return wrapped
-
+import matplotlib.pyplot as plt
+from matplotlib import rc
 
 
 def gauge_timeseries(gauge, dat) :
@@ -16,3 +12,40 @@ def gauge_timeseries(gauge, dat) :
         outfile.write(str(dat[i]) + '\n')
 
     outfile.close()
+
+
+
+def plot_gauges(gauge) :
+    """A function for plotting timeseries data on a single axis."""
+
+    curves = {1 : coarse, 2 : fine, 3 : simple, 4 : adjoint}
+    plt.rc('text', usetex = True)
+    plt.rc('font', family = 'serif')
+
+    for key in curves :
+
+        val = []
+        infile = open('timeseries/{y1}_{y2}.txt'.format(y1 = gauge, y2 = curves[key]), r)
+
+        for line in infile :
+            val.append(float(line))
+
+        infile.close()
+
+        # Plot gauge time series:
+        plt.plot(np.linspace(0, 60, len(val)), val)
+
+    plt.gcf().subplots_adjust(bottom = 0.15)
+    plt.ylim([-5, 5])
+    plt.legend()
+    plt.xlabel(r'Time elapsed (mins)')
+    plt.ylabel(r'Free surface (m)')
+    plt.savefig('plots/tsunami_outputs/screenshots/full_gauge_timeseries_{y}.png'.format(y = gauge))
+
+
+
+def wrapper(func, *args, **kwargs) :
+    """A wrapper function to enable timing of functions with arguments"""
+    def wrapped() :
+        return func(*args, **kwargs)
+    return wrapped
